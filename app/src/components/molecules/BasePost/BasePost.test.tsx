97e661fa4b1df8056a649440edd034b611e24681
tk1351 <takakikitsui0111@gmail.com>
2022-01-28 21:24:51 +0900

    fix: BasePostのテストリファクタリング

--- app/src/components/molecules/BasePost/BasePost.test.tsx
+++ app/src/components/molecules/BasePost/BasePost.test.tsx
@@ -1,57 +1,44 @@
 import { render, screen } from '@testing-library/react'
 import BasePost from './BasePost'
 
+const setup = () => {
+  const utils = render(
+    <BasePost title={'title'} date={'2022-01-01'} description={'description'} />
+  )
+  const post = utils.getByLabelText('post')
+  const title = utils.getByRole('heading')
+  const date = utils.getByLabelText('post-date')
+  const description = utils.getByLabelText('post-date')
+  return {
+    ...utils,
+    post,
+    title,
+    date,
+    description,
+  }
+}
+
 describe('BasePost', () => {
   it('headingが存在すること', () => {
-    render(
-      <BasePost
-        title={'title'}
-        date={'2022-01-01'}
-        description={'description'}
-      />
-    )
-    expect(screen.getByRole('heading')).toBeInTheDocument()
+    const { title } = setup()
+    expect(title).toBeInTheDocument()
   })
   it('dateが存在すること', () => {
-    render(
-      <BasePost
-        title={'title'}
-        date={'2022-01-01'}
-        description={'description'}
-      />
-    )
-    expect(screen.getByLabelText('post-date')).toBeInTheDocument()
+    const { date } = setup()
+    expect(date).toBeInTheDocument()
   })
   it('descriptionが存在すること', () => {
-    render(
-      <BasePost
-        title={'title'}
-        date={'2022-01-01'}
-        description={'description'}
-      />
-    )
-    expect(screen.getByLabelText('description')).toBeInTheDocument()
+    const { description } = setup()
+    expect(description).toBeInTheDocument()
   })
   it('正しくpropsが渡っていること', () => {
-    render(
-      <BasePost
-        title={'title'}
-        date={'2022-01-01'}
-        description={'description'}
-      />
-    )
+    setup()
     expect(screen.getByText('title')).toBeInTheDocument()
     expect(screen.getByText('2022-01-01')).toBeInTheDocument()
     expect(screen.getByText('description')).toBeInTheDocument()
   })
   it('classが正しく付与されていること', () => {
-    render(
-      <BasePost
-        title={'title'}
-        date={'2022-01-01'}
-        description={'description'}
-      />
-    )
-    expect(screen.getByLabelText('post')).toHaveClass('post')
+    const { post } = setup()
+    expect(post).toHaveClass('post')
   })
 })
