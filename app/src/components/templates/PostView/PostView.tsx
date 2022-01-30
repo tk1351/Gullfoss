d967743b56851996bc9b863bb2b3bbaf2172b17e
tk1351 <takakikitsui0111@gmail.com>
2022-01-31 07:43:25 +0900

    feat: PostViewのレイアウト追加

--- app/src/components/templates/PostView/PostView.tsx
+++ app/src/components/templates/PostView/PostView.tsx
@@ -1,7 +1,20 @@
 import { VFC } from 'react'
+import BaseHeading1 from '../../atoms/BaseHeading1/BaseHeading1'
+import styles from './PostView.module.css'
+import BaseDescription from '../../atoms/BaseDescription/BaseDescription'
+import BaseDate from '../../atoms/BaseDate/BaseDate'
+import BaseTags from '../../molecules/BaseTags/BaseTags'
 
 const PostView: VFC = () => {
-  return <p>Post View</p>
+  const { wrapper } = styles
+  return (
+    <div className={wrapper}>
+      <BaseHeading1 text={'Lorem Ipsum'} />
+      <BaseDate date={'2022-01-01'} />
+      <BaseTags tags={['React', 'Next.js']} />
+      <BaseDescription text={'Lorem Ipsum'} />
+    </div>
+  )
 }
 
 export default PostView
